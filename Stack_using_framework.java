19dca20cd5b4a8091f05bfa4e21b6bdc69250f1f
Hritvik Bhatia <hritvik111@gmail.com>
2024-09-21 08:44:19 +0530

    stack questions

--- Stack_using_framework.java
+++ Stack_using_framework.java
@@ -2,6 +2,81 @@ import java.util.Stack;
 
 public class Stack_using_framework {
 
+    public static void pushatBottom(Stack<Integer> s,int data){
+        if(s.isEmpty()){
+            s.push(data);
+            return;
+        }
+
+        int top = s.pop();
+        pushatBottom(s, data);
+        s.push(top);
+    }
+
+    public static void reverseStack(Stack<Integer> s){
+        if(s.isEmpty()){
+            return;
+        }
+
+        int top = s.pop();
+        reverseStack(s);
+        pushatBottom(s, top);
+    }
+
+    public static void printS(Stack<Integer> s){
+        while (!s.isEmpty()) {
+            System.out.println(s.pop());
+        }
+    }
+
+    public static String reverseString(String str){
+        Stack<Character> s = new Stack<>();
+        int idx = 0;
+        while(idx < str.length()){
+            s.push(str.charAt(idx));
+            idx++;
+        }
+
+        StringBuilder result = new StringBuilder();
+        while (!s.isEmpty()) {
+            char curr = s.pop();
+            result.append(curr);
+        }
+
+        return result.toString();
+    }
+
+    public static boolean isValid(String str){
+        Stack<Character> s = new Stack<>();
+        
+        for(int i=0; i<str.length(); i++ ){
+            char ch = str.charAt(i);
+
+            //opening
+            if(ch=='(' || ch =='{' || ch=='[' || ch=='<'){
+                s.push(ch);
+            }else{
+                //closing 
+                if(s.isEmpty()){
+                    return false;
+                }
+                if(s.peek() == '(' && ch==')'
+                    || s.peek() == '{' && ch=='}'
+                    || s.peek() == '[' && ch==']'
+                    || s.peek() == '<' && ch=='>'){
+                        s.pop();
+                }else{
+                    return false;
+                }
+            }
+        }
+        if(s.isEmpty()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
     public static void main(String[] args) {
         Stack<Integer> s = new Stack<>();
         s.push(1);   
@@ -9,8 +84,17 @@ public class Stack_using_framework {
         s.push(3);   
         s.push(4);
         
-        while( !s.isEmpty()){
-            System.out.println(s.pop());
-        }
+        // pushatBottom(s, 5);
+        reverseStack(s);
+
+        printS(s);
+
+        // //reverse a string
+        // String str = "abcd";
+        // System.out.println(reverseString(str));
+
+        // //parentheses validation
+        // String str = "({})[]";
+        // System.out.println(isValid(str)); 
     }
 }
